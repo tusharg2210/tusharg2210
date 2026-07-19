f8954f483bc1e1a17ffd4d374a144d5002b29f1c
Tushar Gupta <tushargjnvspn@gmail.com>
2026-07-19 11:06:26 +0530

    add-analytics

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,8 @@ import Achievements from "./components/Achievements";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
+import { Analytics } from "@vercel/analytics/react"
+
 function App() {
   return (
     <SmoothScroll>
